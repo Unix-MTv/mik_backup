e4096e84437e1662443bdde6a4d076c993071b14
MTv <viktorspeed@yandex.ru>
2021-01-17 05:18:00 +0300

    Update mik_backup.sh

--- mik_backup.sh
+++ mik_backup.sh
@@ -7,15 +7,15 @@ login="username"
 #passwd="pa$Sw0rd"
 fulldir="${backupdir}/`date +%Y`/`date +%m`/`date +%d`"
 
-# Функция проверки хостов;
-check_host() {
-	ping -c 3 ${r} > /dev/null
-}
-
 # Условие для проверки наличия директории и ее очистки;
 if [ -d $backupdir ]; then
 	find ${backupdir}/* -type d -mtime +${backupage} -exec rm -rf {} \; > /dev/null
 fi
+
+# Функция проверки хостов;
+check_host() {
+	ping -c 3 ${r} > /dev/null
+}
 
 # Функция создание бекапа;
 create_backup() {
